80849d391cdaa1ed7049c6bc8ea1f37d526cd04f
Apeksha-varpe07 <apekshavarpe061@gmail.com>
2026-02-25 20:40:14 +0530

    Create BSTcnt.c

---
+++ BSTcnt.c
@@ -0,0 +1,98 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct node {
+    int data;
+    struct node *left;
+    struct node *right;
+};
+
+struct node *createNode(int value) {
+    struct node *newNode = malloc(sizeof(struct node));
+    newNode->data = value;
+    newNode->left = NULL;
+    newNode->right = NULL;
+    return newNode;
+}
+
+struct node *insert(struct node *root, int value) {
+    if (root == NULL) {
+        return createNode(value);
+    }
+    if (value < root->data) {
+        root->left = insert(root->left, value);
+    } else if (value > root->data) {
+        root->right = insert(root->right, value);
+    }
+    return root;
+}
+
+void inorderTraversal(struct node *root) {
+    if (root != NULL) {
+        inorderTraversal(root->left);
+        printf("%d -> ", root->data);
+        inorderTraversal(root->right);
+    }
+}
+
+int countNodes(struct node *root) {
+    if (root == NULL) {
+        return 0;
+    }
+    return 1 + countNodes(root->left) + countNodes(root->right);
+}
+
+int countLeafNodes(struct node *root) {
+    if (root == NULL) {
+        return 0;
+    }
+    if (root->left == NULL && root->right == NULL) {
+        return 1;
+    }
+    return countLeafNodes(root->left) + countLeafNodes(root->right);
+}
+
+int main() {
+    struct node *root = NULL;
+    int choice, value, nodeCount, leafCount;
+    while (1) {
+        printf("\nBinary Search Tree Operations\n");
+        printf("1. Insert element\n");
+        printf("2. Count total number of nodes\n");
+        printf("3. Count total number of leaf nodes\n");
+        printf("4. Inorder traversal\n");
+        printf("5. Exit\n");
+        printf("Enter your choice: ");
+        scanf("%d", &choice);
+        switch (choice) {
+            case 1:
+                printf("Enter the value to be inserted: ");
+                scanf("%d", &value);
+                root = insert(root, value);
+                break;
+            case 2:
+                nodeCount = countNodes(root);
+                printf("Total number of nodes: %d\n", nodeCount);
+                break;
+            case 3:
+                leafCount = countLeafNodes(root);
+                printf("Total number of leaf nodes: %d\n", leafCount);
+                break;
+            case 4:
+                printf("Inorder traversal: ");
+                inorderTraversal(root);
+                printf("\n");
+                break;
+            case 5:
+                printf("Exiting...\n");
+                exit(0);
+            default:
+                printf("Invalid choice. Please try again.\n");
+        }
+    }
+    return 0;
+}
+
+
+
+
