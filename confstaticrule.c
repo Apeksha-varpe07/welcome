da7d8d1f68c210f26d29ae80589d9b67ffd3c1b0
Apeksha-varpe07 <apekshavarpe061@gmail.com>
2026-03-06 20:17:57 +0530

    Create confstaticrule.c
    2. Write a program to configure static route between multiple networks.

---
+++ confstaticrule.c
@@ -0,0 +1,11 @@
+#include <stdio.h> 
+int main() { 
+printf("Static Route Configuration\n"); 
+printf("Destination Network : 192.168.2.0\n"); 
+printf("Subnet Mask  
+printf("Gateway 
+: 255.255.255.0\n"); 
+: 10.0.0.2\n"); 
+printf("Route Added Successfully!\n"); 
+return 0; 
+}
