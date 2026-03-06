f0ccf295973a28c2d7b8491f1bca125a496de34a
Apeksha-varpe07 <apekshavarpe061@gmail.com>
2026-03-06 20:19:09 +0530

    Create  dynamic routing protocol.c
    3. Write a program to set up a dynamic routing protocols such as RIP,EIGRP, or OSPF

---
+++ dynamic routing protocol.c
@@ -0,0 +1,8 @@
+#include <stdio.h> 
+int main() { 
+printf("Dynamic Routing Enabled\n"); 
+printf("Protocol : RIP Version 2\n"); 
+printf("Advertising Networks : 192.168.1.0 , 192.168.2.0\n"); 
+printf("Routing Table Updated Successfully!\n"); 
+return 0; 
+}
