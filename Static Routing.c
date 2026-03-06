fd596d2d67b7ddc04f37244cdd10ba06d2e48220
Apeksha-varpe07 <apekshavarpe061@gmail.com>
2026-03-06 20:20:51 +0530

    Create  Static Routing.c
    5. Write a program to implements Static Routing Simulation.

---
+++ Static Routing.c
@@ -0,0 +1,8 @@
+#include <stdio.h> 
+int main() { 
+int routes = 3; 
+printf("Static Routing Simulation\n"); 
+printf("Routes Configured = %d\n", routes); 
+printf("All Networks Reachable!\n"); 
+return 0; 
+} 
