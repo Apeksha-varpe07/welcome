7cc01c5c85035fd8277cf3b5a7135df674f92df8
Apeksha-varpe07 <apekshavarpe061@gmail.com>
2026-03-07 20:36:32 +0530

    Create addthreeno.py
    addition of three number

---
+++ addthreeno.py
@@ -0,0 +1,7 @@
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+c = int(input("Enter third number: "))
+
+sum = a + b + c
+
+print("Addition of three numbers is:", sum)
