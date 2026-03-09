4c1f10efa4ada4c1fbb06008836803438a5c4e69
Apeksha-varpe07 <apekshavarpe061@gmail.com>
2026-03-09 20:45:41 +0530

    Create all opreator.py
    addition subtraction multiplication and division program in python language

---
+++ all opreator.py
@@ -0,0 +1,20 @@
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+
+print("1.Addition")
+print("2.Subtraction")
+print("3.Multiplication")
+print("4.Division")
+
+choice = int(input("Enter your choice: "))
+
+if choice == 1:
+    print("Addition =", a + b)
+elif choice == 2:
+    print("Subtraction =", a - b)
+elif choice == 3:
+    print("Multiplication =", a * b)
+elif choice == 4:
+    print("Division =", a / b)
+else:
+    print("Invalid choice")
