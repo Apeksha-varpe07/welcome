942c5dbfa71c9b400e4e5892c7b71d61b8038403
Apeksha-varpe07 <apekshavarpe061@gmail.com>
2026-03-09 20:46:34 +0530

    Create studentmarks.py
    student marks program in python language

---
+++ studentmarks.py
@@ -0,0 +1,9 @@
+m1 = int(input("Enter marks of subject 1: "))
+m2 = int(input("Enter marks of subject 2: "))
+m3 = int(input("Enter marks of subject 3: "))
+
+total = m1 + m2 + m3
+avg = total / 3
+
+print("Total =", total)
+print("Average =", avg)
