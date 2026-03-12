72d5a4af7bee7ca433b19750ad2f8267fae9315a
Apeksha-varpe07 <apekshavarpe061@gmail.com>
2026-03-12 20:14:35 +0530

    Create cubeno.c

---
+++ cubeno.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    int num, cube;
+    printf("Enter number: ");
+    scanf("%d", &num);
+
+    cube = num * num * num;
+
+    printf("Cube = %d", cube);
+    return 0;
+}
