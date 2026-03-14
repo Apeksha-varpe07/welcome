acbe4fa2809d761d3159a3b51d6b6ce5bcfc6b07
Apeksha-varpe07 <apekshavarpe061@gmail.com>
2026-03-14 20:00:25 +0530

    Create filehandadc.c
    this is advanced c prpogram
     File Handling (Write Data to File)

---
+++ filehandadc.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+    FILE *fp;
+    char text[100];
+
+    fp = fopen("data.txt", "w");
+
+    printf("Enter text: ");
+    gets(text);
+
+    fprintf(fp, "%s", text);
+    fclose(fp);
+
+    printf("Data written to file successfully.");
+
+    return 0;
+}
