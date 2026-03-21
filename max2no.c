f54ef7d739edf6f9aeac60156fbaf9d00014fc34
Apeksha-varpe07 <apekshavarpe061@gmail.com>
2026-03-21 12:16:05 +0530

    Create max2no.c
    maximum between two number

---
+++ max2no.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+int a,b;
+printf("enter your values of a");
+scanf("%d",&a);
+printf("enter your values of b");
+scanf("%d",&b);
+if(a>b)
+{
+printf("maximum number is=%d",a);
+}
+else
+{
+printf("maximum number is=%d",b);
+}
+return 0;
+}
