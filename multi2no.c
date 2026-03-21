48e3fb741e3af54c2b59c1a55d661c849bcb8018
Apeksha-varpe07 <apekshavarpe061@gmail.com>
2026-03-21 12:13:19 +0530

    Create multi2no.c
    multiplication of two number

---
+++ multi2no.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+int a,b,c;
+printf("enter your values of a");
+scanf("%d",&a);
+printf("enter your values of b");
+scanf("%d",&b);
+c=a*b;
+printf("multilplication of two number:%d",c);
+return 0;
+}
