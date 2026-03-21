42e1d95b9b537fe29337ac0e7da5d17cd6185ea0
Apeksha-varpe07 <apekshavarpe061@gmail.com>
2026-03-21 12:17:52 +0530

    Create min2no.c
    minimum between two number

---
+++ min2no.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+int a,b;
+printf("enter your values of a");
+scanf("%d",&a);
+printf("enter your values of b");
+scanf("%d",&b);
+if(a<b)
+{
+printf("minimum number is=%d",a);
+}
+else
+{
+printf("minimum number is=%d",b);
+}
+return 0;
+}
