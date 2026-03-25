f53a9bac719bd1e8751d84f0726fe09c28aeb0e0
Apeksha-varpe07 <apekshavarpe061@gmail.com>
2026-03-25 18:18:43 +0530

    Create productinfo.py

---
+++ productinfo.py
@@ -0,0 +1,6 @@
+def prodect info (product_name,price=100):
+   if (price<100)
+print(f"product:{product_name},price:rs.{price}")
+else:
+   print("product is not less than rs.100")
+productInfo("pen",120)
