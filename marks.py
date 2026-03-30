a148bb641f3a955fc0e3ecbe8bd4808d1c2d3196
Apeksha-varpe07 <apekshavarpe061@gmail.com>
2026-03-30 14:03:52 +0530

    Create marks.py

---
+++ marks.py
@@ -0,0 +1,8 @@
+marks={}
+sub1=int(input("enter a phy:"))
+sub2=int(input("enter a chem:"))
+sub3=int(input("enter a math:"))
+marks.update({"phy":sub1})
+marks.update({"chem":sub2})
+marks.update({"math":sub3})
+print(marks)
