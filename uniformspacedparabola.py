bd545cc87f3415063480301f384174f37bac105a
Apeksha-varpe07 <apekshavarpe061@gmail.com>
2026-04-04 20:39:25 +0530

    Create uniformspacedparabola.py

---
+++ uniformspacedparabola.py
@@ -0,0 +1,21 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+n = 20
+x_min = -10
+x_max = 10
+
+# Generate uniformly spaced x values
+x = np.linspace(x_min, x_max, n)
+
+# Parabola equation
+y = x**2
+
+# Plotting
+plt.plot(x, y, 'o-', label='Uniformly spaced points')
+plt.xlabel('x')
+plt.ylabel('y')
+plt.title('Uniformly spaced points on parabola y = x^2')
+plt.grid(True)
+plt.legend()
+plt.show()
