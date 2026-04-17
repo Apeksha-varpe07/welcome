a3f024b0172673da3051e5e6c3d332d529ed3b2f
Apeksha-varpe07 <apekshavarpe061@gmail.com>
2026-04-17 22:21:42 +0530

    Create Circle (x² + y² = 49).py

---
+++ Circle (x² + y² = 49).py
@@ -0,0 +1,14 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+r = 7
+theta = np.linspace(0, 2*np.pi, 100)
+
+x = r * np.cos(theta)
+y = r * np.sin(theta)
+
+plt.plot(x, y)
+plt.gca().set_aspect('equal')
+
+plt.title("Circle x^2 + y^2 = 49")
+plt.show()
