062fdfdd68d0ecf8d2366b354f1f270a7af4e80a
Apeksha-varpe07 <apekshavarpe061@gmail.com>
2026-04-19 19:13:28 +0530

    Create wireframe plot.py

---
+++ wireframe plot.py
@@ -0,0 +1,15 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+x = np.linspace(-20, 20, 50)
+y = np.linspace(-20, 20, 50)
+X, Y = np.meshgrid(x, y)
+
+Z = X**2 - Y**2
+
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+
+ax.plot_wireframe(X, Y, Z, color='brown')
+
+plt.show()
