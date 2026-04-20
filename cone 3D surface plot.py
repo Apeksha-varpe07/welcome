293ff10c9e31766360be5c5367347d0a7a11bbf8
Apeksha-varpe07 <apekshavarpe061@gmail.com>
2026-04-20 19:30:27 +0530

    Create cone 3D surface plot.py

---
+++ cone 3D surface plot.py
@@ -0,0 +1,15 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+x = np.linspace(-10, 10, 50)
+y = np.linspace(-10, 10, 50)
+X, Y = np.meshgrid(x, y)
+
+Z = np.sqrt(X**2 + Y**2)
+
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+
+ax.plot_surface(X, Y, Z, color='purple')
+
+plt.show()
