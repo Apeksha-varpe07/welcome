8ad765aaf2af3f606a91bb40bcdb2bc90ebbc8f4
Apeksha-varpe07 <apekshavarpe061@gmail.com>
2026-05-12 16:17:33 +0530

    Create program to copy one file into another.c

---
+++ program to copy one file into another.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+    FILE *fp1, *fp2;
+    char ch;
+
+    fp1 = fopen("source.txt", "r");
+    fp2 = fopen("target.txt", "w");
+
+    while((ch = fgetc(fp1)) != EOF) {
+        fputc(ch, fp2);
+    }
+
+    printf("File copied successfully.");
+
+    fclose(fp1);
+    fclose(fp2);
+
+    return 0;
+}
