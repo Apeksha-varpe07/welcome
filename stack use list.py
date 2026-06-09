cc3a2b05acac7deeb9f69b5d9e62f2707ae9ea08
Apeksha-varpe07 <apekshavarpe061@gmail.com>
2026-06-09 23:10:30 +0530

    Create stack use list.py

---
+++ stack use list.py
@@ -0,0 +1,14 @@
+stack = []
+
+# Push
+stack.append(10)
+stack.append(20)
+stack.append(30)
+
+print("Stack after Push:", stack)
+
+# Pop
+item = stack.pop()
+print("Popped Item:", item)
+
+print("Stack after Pop:", stack)
