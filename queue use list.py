0af67a9844b9413f0be6b0f58ed92affd399a46a
Apeksha-varpe07 <apekshavarpe061@gmail.com>
2026-06-09 23:11:29 +0530

    Create queue use list.py

---
+++ queue use list.py
@@ -0,0 +1,14 @@
+queue = []
+
+# Enqueue
+queue.append(10)
+queue.append(20)
+queue.append(30)
+
+print("Queue after Enqueue:", queue)
+
+# Dequeue
+item = queue.pop(0)
+print("Dequeued Item:", item)
+
+print("Queue after Dequeue:", queue)
