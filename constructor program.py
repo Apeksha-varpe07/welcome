5d07b967fa9c37a634e789c6bdf2f8b3df33e8e5
Apeksha-varpe07 <apekshavarpe061@gmail.com>
2026-08-08 19:55:52 +0530

    Create constructor program.py

---
+++ constructor program.py
@@ -0,0 +1,15 @@
+class Student:
+    def __init__(self):
+        self.name = "Apeksha"
+        self.age = 20
+
+    def display(self):
+        print("Name:", self.name)
+        print("Age:", self.age)
+
+
+# Create object
+s1 = Student()
+
+# Display details
+s1.display()
