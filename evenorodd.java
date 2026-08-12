6416b6e7d409b07df864ff4319df174787fe3fd5
Apeksha-varpe07 <apekshavarpe061@gmail.com>
2026-08-12 20:07:00 +0530

    Create evenorodd.java

---
+++ evenorodd.java
@@ -0,0 +1,12 @@
+class EvenOdd
+{
+    public static void main(String[] args)
+    {
+        int n = 12;
+
+        if(n % 2 == 0)
+            System.out.println("Even Number");
+        else
+            System.out.println("Odd Number");
+    }
+}
