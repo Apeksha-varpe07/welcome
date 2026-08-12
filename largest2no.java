ef4e851d13a26a05a5392a541acd76b728feb695
Apeksha-varpe07 <apekshavarpe061@gmail.com>
2026-08-12 20:08:13 +0530

    Create largest2no.java

---
+++ largest2no.java
@@ -0,0 +1,12 @@
+class Largest
+{
+    public static void main(String[] args)
+    {
+        int a = 25, b = 15;
+
+        if(a > b)
+            System.out.println("A is Largest");
+        else
+            System.out.println("B is Largest");
+    }
+}
